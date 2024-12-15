799a3f1edc13d71ec8840b6d951b40e439db9f29
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-12-15 23:14:53 +0000

    fix: New option to disable the scheduler

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ from src.core import BridgeClient
 from src.settings import config
 
 
+def run():
+    bridge = BridgeClient(
+        # General
+        partial_scan=config.PARTIAL_SCAN,
+        destructive_sync=config.DESTRUCTIVE_SYNC,
+        # Anilist
+        anilist_token=config.ANILIST_TOKEN,
+        anilist_user=config.ANILIST_USER,
+        # Plex
+        plex_url=config.PLEX_URL,
+        plex_token=config.PLEX_TOKEN,
+        plex_sections=config.PLEX_SECTIONS,
+        plex_user=config.PLEX_USER,
+        # Advanced
+        dry_run=config.DRY_RUN,
+        fuzzy_search_threshold=config.FUZZY_SEARCH_THRESHOLD,
+    )
+    bridge.sync()
+
+
 def schedule_sync(scheduler: sched.scheduler):
     try:
-        bridge = BridgeClient(
-            # General
-            partial_scan=config.PARTIAL_SCAN,
-            destructive_sync=config.DESTRUCTIVE_SYNC,
-            # Anilist
-            anilist_token=config.ANILIST_TOKEN,
-            anilist_user=config.ANILIST_USER,
-            # Plex
-            plex_url=config.PLEX_URL,
-            plex_token=config.PLEX_TOKEN,
-            plex_sections=config.PLEX_SECTIONS,
-            plex_user=config.PLEX_USER,
-            # Advanced
-            dry_run=config.DRY_RUN,
-            fuzzy_search_threshold=config.FUZZY_SEARCH_THRESHOLD,
-        )
-        bridge.sync()
+        run()
     except Exception as e:
         log.error("Scheduler: Failed to sync", exc_info=e)
     finally:
@@ -40,10 +44,16 @@ def schedule_sync(scheduler: sched.scheduler):
 if __name__ == "__main__":
     log.info(f"PlexAniBridge: [CONFIG] => {config}")
 
-    s = sched.scheduler(time.time, time.sleep)
-    s.enterabs(time.time(), 1, schedule_sync, (s,))
+    if config.SYNC_INTERVAL == -1:
+        log.info(
+            "Scheduler: `SYNC_INTERVAL` is set to -1, disabling the scheduler. The script will run once and exit."
+        )
+        run()
+    else:
+        s = sched.scheduler(time.time, time.sleep)
+        s.enterabs(time.time(), 1, schedule_sync, (s,))
 
-    try:
-        s.run()
-    except KeyboardInterrupt:
-        pass
+        try:
+            s.run()
+        except KeyboardInterrupt:
+            pass
