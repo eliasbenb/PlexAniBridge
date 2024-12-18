6ec5dd3da648dc3367744c723f7d4170ccb42ee0
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-12-18 05:26:50 +0000

    fix: SQLAlchemy JSON `sa_type` param

--- src/models/animap.py
+++ src/models/animap.py
@@ -9,9 +9,9 @@ class AniMap(SQLModel, table=True):
     __tablename__ = "animap"
 
     anidb_id: int = Field(primary_key=True)
-    anilist_id: Optional[list[int]] = Field(sa_type=JSON(int), index=True)
-    imdb_id: Optional[list[str]] = Field(sa_type=JSON(str), index=True)
-    mal_id: Optional[list[int]] = Field(sa_type=JSON(int), index=True)
+    anilist_id: Optional[list[int]] = Field(sa_type=JSON, index=True)
+    imdb_id: Optional[list[str]] = Field(sa_type=JSON, index=True)
+    mal_id: Optional[list[int]] = Field(sa_type=JSON, index=True)
     tmdb_movie_id: Optional[int] = Field(index=True)
     tmdb_show_id: Optional[int] = Field(index=True)
     tvdb_epoffset: Optional[int]
