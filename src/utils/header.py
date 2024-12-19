910f351d35954ef495b47573a2497ea95b121d9a
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-12-19 08:39:58 +0000

    docs: Include the maintainer in the program header

--- src/utils/header.py
+++ src/utils/header.py
@@ -1,4 +1,4 @@
-from src import __author__, __git_hash__, __license__, __version__
+from src import __author__, __git_hash__, __license__, __maintainer__, __version__
 
 plexanibridge_header = f"""
 ╔═══════════════════════════════════════════════════════════════════════════════╗
@@ -7,7 +7,7 @@ plexanibridge_header = f"""
 ║                                                                               ║
 ║  Version: {__version__:<68}║
 ║  Git Hash: {__git_hash__:<67}║
-║  Author: {__author__:<69}║
+║  Author: {f"{__author__ } @{__maintainer__}":<69}║
 ║  License: {__license__:<68}║
 ║  Repository: https://github.com/eliasbenb/PlexAniBridge                       ║
 ║                                                                               ║
