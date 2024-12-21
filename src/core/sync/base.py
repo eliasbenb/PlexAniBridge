51c6f038ec703f997f1c08dbdb8dc4ba88e059d1
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-12-21 22:20:42 +0000

    fix: Variable name reference change

--- src/core/sync/base.py
+++ src/core/sync/base.py
@@ -214,7 +214,7 @@ class BaseSyncClient(ABC, Generic[T, S]):
             id=anilist_media.media_list_entry
             and anilist_media.media_list_entry.id
             or -1,
-            user_id=self.anilist_client.anilist_user.id,
+            user_id=self.anilist_client.user.id,
             media_id=anilist_media.id,
             status=self._calculate_status(item, subitem, anilist_media, animapping),
             score=self._calculate_score(item, subitem, anilist_media, animapping),
