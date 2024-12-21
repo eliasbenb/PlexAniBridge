35680c6d54fe3560affdfe69f7d35427eb613122
Elias Benbourenane <eliasbenbourenane@gmail.com>
2024-12-21 02:34:42 +0000

    fix: Incorrectly initialized `EXCLUDED_SYNC_FIELDS`

--- src/settings.py
+++ src/settings.py
@@ -68,7 +68,7 @@ class PlexAnibridgeConfig(BaseSettings):
     PARTIAL_SCAN: bool = True
     DESTRUCTIVE_SYNC: bool = False
 
-    EXCLUDED_SYNC_FIELDS: set[SyncField] = {}
+    EXCLUDED_SYNC_FIELDS: set[SyncField] = set()
 
     # Advanced
     DATA_PATH: Path = "./data"
