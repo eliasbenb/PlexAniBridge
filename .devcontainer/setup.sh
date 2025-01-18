1019e87549302041c81930612c09fda42453bbef
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-01-18 03:04:02 +0000

    chore: Setup mkdocs dev environment for the devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,3 +2,11 @@
 
 sudo apt-get update && sudo apt-get install -y sqlite3
 pip3 install --user -r requirements.txt
+
+python3 -m venv docs/.venv
+source docs/.venv/bin/activate
+pip3 install -r docs/requirements.txt
+deactivate
+
+# add an alias for mkdocs in the .bashrc file
+echo "alias mkdocs='docs/.venv/bin/python3 -m mkdocs'" >> ~/.bashrc
