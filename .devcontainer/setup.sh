5dbe9cbd3da6c8705346657c57a8c3f92508fe73
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-02-11 18:37:05 +0000

    chore: Install Docker and act in the devcontainer for GH actions testing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# install dependencies
 sudo apt-get update && sudo apt-get install -y sqlite3
-pip3 install --user -r requirements.txt
+pip3 install -r requirements.txt
 
+# install mkdocs
 python3 -m venv docs/.venv
 source docs/.venv/bin/activate
 pip3 install -r docs/requirements.txt
 deactivate
-
-# add an alias for mkdocs in the .bashrc file
 echo "alias mkdocs='docs/.venv/bin/python3 -m mkdocs'" >> ~/.bashrc
+
+# install act
+mkdir /tmp/act && cd /tmp/act
+curl -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
+mv bin/act ~/.local/bin
+cd ~ && rm -rf /tmp/act
