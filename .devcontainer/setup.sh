82c196cfe2a100e39ecfc2f70101a10edbcc9109
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-03-31 21:56:00 +0000

    chore: Improved devcontainer setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,16 +4,4 @@
 sudo apt-get update && sudo apt-get install -y sqlite3
 
 # Install Python Dependencies
-uv sync
-
-# Install act
-mkdir /tmp/act && cd /tmp/act
-curl -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
-mv bin/act ~/.local/bin
-cd ~ && rm -rf /tmp/act
-
-# Disable hardlinking for uv
-echo "export UV_LINK_MODE=copy" >> ~/.bashrc
-
-# Create an alias for mkdocs
-echo "alias mkdocs='uv run --package docs mkdocs'" >> ~/.bashrc
+uv sync --all-packages
