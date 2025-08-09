45ccaaf4014a73d8576d6f90d5f9aa4b0d269dce
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-08-09 05:12:48 +0000

    feat: Connect all API routers together

--- src/web/routes/api/__init__.py
+++ src/web/routes/api/__init__.py
@@ -2,6 +2,16 @@
 
 from fastapi import APIRouter
 
+from src.web.routes.api.history import router as history_router
+from src.web.routes.api.mappings import router as mappings_router
+from src.web.routes.api.status import router as status_router
+from src.web.routes.api.sync import router as sync_router
+
 __all__ = ["router"]
 
 router = APIRouter()
+
+router.include_router(history_router, prefix="/history", tags=["history"])
+router.include_router(mappings_router, prefix="/mappings", tags=["mappings"])
+router.include_router(status_router, prefix="/status", tags=["status"])
+router.include_router(sync_router, prefix="/sync", tags=["sync"])
