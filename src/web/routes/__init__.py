8bb9bc0f91be4f675bff7aa58c72ee56377b83d0
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-08-10 23:12:12 +0000

    feat: Plex webhook API route

--- src/web/routes/__init__.py
+++ src/web/routes/__init__.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter
 
 from src.web.routes.api import router as api_router  # isort:skip
+from src.web.routes.webhook import router as webhook_router  # isort:skip
 from src.web.routes.ws import router as ws_router  # isort:skip
 
 from src.web.routes.dashboard import router as dashboard_router
@@ -21,5 +22,6 @@ page_router.include_router(mappings_router, tags=["mappings"])
 page_router.include_router(system_router, tags=["system"])
 
 router.include_router(api_router, prefix="/api", tags=["api"])
+router.include_router(webhook_router, prefix="/webhook", tags=["webhook"])
 router.include_router(ws_router, prefix="/ws", tags=["ws"])
 router.include_router(page_router, tags=["pages"])
