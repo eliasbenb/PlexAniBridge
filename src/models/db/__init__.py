618b2d539c94c851fe83e7f8f372f0b54320b40d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-09-19 00:36:17 +0000

    feat: Provenance database model

--- src/models/db/__init__.py
+++ src/models/db/__init__.py
@@ -3,6 +3,13 @@
 from src.models.db.animap import AniMap
 from src.models.db.base import Base
 from src.models.db.housekeeping import Housekeeping
+from src.models.db.provenance import AniMapProvenance
 from src.models.db.sync_history import SyncHistory
 
-__all__ = ["AniMap", "Base", "Housekeeping", "SyncHistory"]
+__all__ = [
+    "AniMap",
+    "AniMapProvenance",
+    "Base",
+    "Housekeeping",
+    "SyncHistory",
+]
